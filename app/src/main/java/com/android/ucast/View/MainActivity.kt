ee5f235c2f2e002143956bd57838a3bf0378b5b0
fahmi54321 <fahmiabdulaziz44@gmail.com>
2021-03-15 11:55:18 +0700

    menambah packet View, Model dan packet ViewModel menambah warna di colors.xml menambah font style beserta size di themes.xml

--- app/src/main/java/com/android/ucast/View/MainActivity.kt
+++ app/src/main/java/com/android/ucast/View/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.android.ucast
+package com.android.ucast.View
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.android.ucast.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
