ab93600aff903de2cacaeefc4f37043cf7941191
fahmi54321 <fahmiabdulaziz44@gmail.com>
2021-03-19 14:05:00 +0700

    menambahkan animasi di layout schedule

--- app/src/main/java/com/android/ucast/View/Schedule/ScheduleFragment.kt
+++ app/src/main/java/com/android/ucast/View/Schedule/ScheduleFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import androidx.lifecycle.lifecycleScope
 import com.android.ucast.Adapter.Schedule.DataAdapter
@@ -29,10 +32,14 @@ class ScheduleFragment : DaggerFragment() {
     lateinit var providerFactory: ViewModelProviderFactory
     private var adapter: DataAdapter? = null
     lateinit var viewModel: ViewModelUCase
-
     lateinit var binding : FragmentScheduleBinding
-
     lateinit var sheetBehavior: BottomSheetBehavior<*>
+
+    // deklarasi animasi
+    lateinit var top_to_bottom : Animation
+    lateinit var bottom_to_top : Animation
+    lateinit var left_to_right : Animation
+    lateinit var right_to_left : Animation
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -46,14 +53,19 @@ class ScheduleFragment : DaggerFragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-        sheetBehavior = BottomSheetBehavior.from(bottomsheett)
-        sheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+        initAnimate()
+        aksiAnimasi()
+        initBottomSheet()
+        initViewModel()
 
-        viewModel =
-            ViewModelProviders.of(this, providerFactory).get(ViewModelUCase::class.java)
         adapter = DataAdapter(object : DataAdapter.onClickListener {
             override fun details(getItem: DataItem?) {
                 sheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+                constraintHeader.startAnimation(left_to_right)
+                linearPenerimaCost.startAnimation(left_to_right)
+                linearTitleCost.startAnimation(left_to_right)
+                linearIsiCost.startAnimation(left_to_right)
+                linearButton.startAnimation(left_to_right)
                 edtPenerimaCast.setText(getItem?.firstName)
                 edtTitleCast.setText(getItem?.lastName)
                 edtIsiCast.setText(getItem?.position)
@@ -61,10 +73,31 @@ class ScheduleFragment : DaggerFragment() {
 
         })
         viewModel?.setData()
-
         setupAdapter()
         startJob()
 
+    }
+
+    private fun aksiAnimasi() {
+        binding.rvSchedule.startAnimation(left_to_right)
+        binding.relativeSchedule.startAnimation(left_to_right)
+    }
+
+    private fun initViewModel() {
+        viewModel =
+                ViewModelProvider(this, providerFactory).get(ViewModelUCase::class.java)
+    }
+
+    private fun initBottomSheet() {
+        sheetBehavior = BottomSheetBehavior.from(bottomsheett)
+        sheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+    }
+
+    private fun initAnimate() {
+        top_to_bottom = AnimationUtils.loadAnimation(context, R.anim.top_to_bottom)
+        bottom_to_top = AnimationUtils.loadAnimation(context, R.anim.bottom_to_top)
+        left_to_right = AnimationUtils.loadAnimation(context, R.anim.left_to_right)
+        right_to_left = AnimationUtils.loadAnimation(context, R.anim.right_to_left)
     }
 
     private fun setupAdapter() {
